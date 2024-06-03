172d90114b37dffa98e7a4492dbb81b3b0945318
Henry <franzhenry46@gmail.com>
2024-06-02 22:40:41 -0300

    fix: arrumando nomeclatura dos metodos de repository-organizations findManyOrganizationId para findByOrganizationId

--- src/repository/organization-repository.ts
+++ src/repository/organization-repository.ts
@@ -4,6 +4,6 @@ import { Organization, Prisma } from "@prisma/client";
 
 export interface OrganizationRepository {
     create(organization: Prisma.OrganizationCreateInput): Promise<Organization>
-    findManyOrganizationId(id: string): Promise<Organization[]>
     findManyOrganizationByCityName(cityName: string): Promise<Organization[] | null>
+    findByOrganizationId(id: string): Promise<Organization | null>
 }
