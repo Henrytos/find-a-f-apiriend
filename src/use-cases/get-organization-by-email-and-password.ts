9f97d30feae320f09730b9529bc28ad82dcfe86b
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-06-03 10:55:41 -0300

    Create get-organization-by-email-and-password.ts

---
+++ src/use-cases/get-organization-by-email-and-password.ts
@@ -0,0 +1,28 @@
+import { OrganizationRepository } from "@/repository/organization-repository";
+import { Organization } from "@prisma/client";
+import { NotFoundOrganizationError } from "./errors/not-found-organition";
+
+interface GetOrganizationByEmailAndPasswordUseCaseRequest {
+    email: string 
+    password: string
+}
+
+interface GetOrganizationByEmailAndPasswordUseCaseResponse {
+    organization: Organization
+}
+
+export class GetOrganizationByEmailAndPasswordUseCase {
+    constructor(private organizationRepository: OrganizationRepository) { }
+
+    async execute({ email, password }: GetOrganizationByEmailAndPasswordUseCaseRequest): Promise<GetOrganizationByEmailAndPasswordUseCase> {
+      const organization = await this.organizationRepository.findByOrganizationByEmailAndPassword({email , password});
+
+        if (!organization) {
+            throw new NotFoundOrganizationError()
+        }
+
+        return {
+            organization
+        };
+    }
+}
