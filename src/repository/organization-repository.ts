296063fa54c2833d1277c13e1574108604604c2d
HENRY <113705213+Henrytos@users.noreply.github.com>
2024-06-03 09:40:41 -0300

    fix:add method findByOrganizationByEmailAndPassword in interface organization-repository.ts
    alterando contrato de classe repositori para fazer sentido caso de uso

--- src/repository/organization-repository.ts
+++ src/repository/organization-repository.ts
@@ -1,9 +1,14 @@
 import { Organization, Prisma } from "@prisma/client";
 
 
+interface findByOrganizationByEmailAndPasswordParams{
+    email:string
+    password:string
+}
 
 export interface OrganizationRepository {
     create(organization: Prisma.OrganizationCreateInput): Promise<Organization>
     findManyOrganizationByCityName(cityName: string): Promise<Organization[] | null>
     findByOrganizationId(id: string): Promise<Organization | null>
+    findByOrganizationByEmailAndPassword({ email, password }:findByOrganizationByEmailAndPasswordParams): Promise<Organization | null>
 }
