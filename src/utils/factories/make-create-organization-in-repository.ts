d0e17aaf8dfd6f615a6169fdb5a8bf077c3b6378
Henry <franzhenry46@gmail.com>
2024-06-17 08:00:53 -0300

    feat:criação de factorie de criação de organização

--- src/utils/factories/make-create-organization-in-repository.ts
+++ src/utils/factories/make-create-organization-in-repository.ts
@@ -2,18 +2,21 @@ import { prisma } from "@/lib/prisma";
 import { Prisma } from "@prisma/client";
 import { faker } from '@faker-js/faker'
 import { hash } from "bcrypt";
+import { randomUUID } from "node:crypto";
 
-interface makeCreateOrganizationParams {
+interface makeCreateOrganizationParams extends Partial<Prisma.OrganizationCreateInput> {
     password: string
     email: string
 }
 
 export async function makeCreateOrganizationInRepository({
     password,
-    email
+    email,
+    ...props
 }: makeCreateOrganizationParams) {
-    await prisma.organization.create({
+    const organization = await prisma.organization.create({
         data: {
+            id: randomUUID(),
             manager_name: faker.person.firstName(),
             email,
             password_hash: await hash(password, 6),
@@ -23,7 +26,9 @@ export async function makeCreateOrganizationInRepository({
             phone: faker.phone.number(),
             roadway: faker.location.country(),
             state: faker.location.state(),
-            role: "ORG"
+            role: "ORG",
+            ...props
         }
     })
+    return { organization }
 }
