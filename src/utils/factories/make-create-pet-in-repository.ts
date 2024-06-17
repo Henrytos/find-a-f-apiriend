c9d562b4a110c14372606b3521bbbc2b27406d51
Henry <franzhenry46@gmail.com>
2024-06-17 08:00:33 -0300

    feat:create factorie de criação de criação de pet

---
+++ src/utils/factories/make-create-pet-in-repository.ts
@@ -0,0 +1,42 @@
+import { prisma } from "@/lib/prisma";
+import { faker } from '@faker-js/faker'
+import { $Enums, Pet } from "@prisma/client";
+import { randomUUID } from "crypto";
+
+interface makeCreatePetInRepositoryParams {
+    organization_id: string,
+    age?: $Enums.PET_AGE,
+    size?: $Enums.PET_SIZE,
+    level_environment?: $Enums.PET_LEVEL_ENVIRONMENT,
+    level_independence?: $Enums.PET_LEVEL_INDEPENDENCE
+}
+
+interface makeCreatePetInRepositoryResponse {
+    pet: Pet
+}
+
+
+export async function makeCreatePetInRepository({
+    organization_id,
+    age,
+    level_environment,
+    level_independence,
+    size
+}: makeCreatePetInRepositoryParams): Promise<makeCreatePetInRepositoryResponse> {
+    const pet = await prisma.pet.create({
+        data: {
+            id: randomUUID(),
+            organization_id,
+            name: faker.animal.dog(),
+            about: faker.lorem.sentence(),
+            age: age ?? 'FILHOTE',
+            level_environment: level_environment ?? 'ALTO',
+            level_independence: level_independence ?? 'ALTO',
+            size: size ?? 'GRANDE',
+            image_url: [faker.image.url()],
+            requirement: [faker.lorem.sentence()],
+        }
+    })
+
+    return { pet }
+}
