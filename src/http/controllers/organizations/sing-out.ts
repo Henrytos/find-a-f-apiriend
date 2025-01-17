eec0ce42ad7b672c91b330bf3841d6869cdfb28c
Henry <franzhenry46@gmail.com>
2025-01-17 00:41:06 -0300

    feat: add refreshToken cookie clearing in sign-out functionality

--- src/http/controllers/organizations/sing-out.ts
+++ src/http/controllers/organizations/sing-out.ts
@@ -3,6 +3,7 @@ import { FastifyReply, FastifyRequest } from "fastify";
 export async function SingOut(request:FastifyRequest, reply:FastifyReply){
 
     reply.setCookie("Authorization", "")
+    reply.setCookie("refreshToken", "")
 
     return reply.status(200).send()
 }
